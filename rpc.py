750fd469436eef38d5dd6efdbf7a3927361bbaaf
Nintendofanboy64 <prcruz89@gmail.com>
2022-03-20 18:20:57 -0700

    Add files via upload

---
+++ rpc.py
@@ -0,0 +1,23 @@
+import random  
+
+weapons =["", "Rock", "Paper", "Scissors"]
+
+print(weapons)
+print("Rock = 1", "Paper = 2", "Scissors = 3")
+
+choice = input("Choose your weapon: ")
+num_choice = int(choice)
+
+computer = random.choice(weapons)
+num_computer = int(weapons.index(computer))
+
+print(weapons[num_choice] + " Vs " + computer)
+
+if num_choice == num_computer:
+    print("Draw")
+
+elif num_choice == 1 and num_computer == 3:
+    print("Win!!!!")
+
+else:
+    print("oops")
