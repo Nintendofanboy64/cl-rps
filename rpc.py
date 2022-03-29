496aacdae59aa97dcb05798cbd8bd21d47ccbfd4
Perry <prcruz89@gmail.com>
2022-03-28 23:57:01 -0700

    added color to the text and fixed the game loop

--- rpc.py
+++ rpc.py
@@ -6,7 +6,7 @@ def game():
 	weapons =["", "Rock", "Paper", "Scissors"]
 	print("")
 	print(weapons[1:])
-	print("\nRock = 1", "Paper = 2", "Scissors = 3\n")
+	print("\033[1;32;40m\nRock = 1", "Paper = 2", "Scissors = 3\n")
 
 	choice = input("Choose your weapon: ")
 	num_choice = int(choice)
@@ -17,29 +17,30 @@ def game():
 	print(weapons[num_choice] + " Vs " + computer)
 
 	if num_choice == num_computer:
-		print("Draw")
+		print("\033[1;36;40mDraw")
 
 	elif num_choice == 1 and num_computer == 3:
-		print("Win!!!!")
+		print("\033[1;33;40mWin!!!!")
 		
 	elif num_choice == 1 and num_computer == 2:
-		print("You Lose :(")
+		print("\033[1;31;40mYou Lose :(")
 
 	elif num_choice == 2 and num_computer == 3:
-		print("You Lose :(")
+		print("\033[1;31;40mYou Lose :(")
 		
 	elif num_choice == 2 and num_computer == 1:
-		print("Win!!!!")
+		print("\033[1;33;40mWin!!!!")
 		
 	elif num_choice == 3 and num_computer == 2:
-		print("Win!!!!")
+		print("\033[1;33;40mWin!!!!")
 		
 	elif num_choice == 3 and num_computer == 1:
-		print("You Lose :(")
+		print("\033[1;31;40mYou Lose :(")
 
 	else:
-		print("oops daisy yay")
-		print("The computer chickened out due to anxiety")
+		print("\033[1;36;40moops daisy yay")
+		print("\033[1;36;40mThe computer chickened out due to anxiety")
+		play()
 
 def play():
 	game = input("Do you want to play? (yes or no): ")
